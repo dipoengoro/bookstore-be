35ccfdc9a10c5c46c2a50572e2214ced3533cba7
Dipoengoro <dipoengoro@outlook.com>
2023-09-09 14:32:34 +0700

    feat: Add route to edit a book by its id

--- index.js
+++ index.js
@@ -59,6 +59,27 @@ app.get("/books/:id", async (req, res) => {
   }
 });
 
+app.put("/books/:id", async (req, res) => {
+  try {
+    if (!req.body.title || !req.body.author || !req.body.publishYear) {
+      return res.status(400).send({
+        message: "Send all required fields: title, author, publishYear.",
+      });
+    }
+
+    const { id } = req.params;
+    const result = await Book.findByIdAndUpdate(id, req.body);
+
+    if (!result) {
+      return res.status(404).json({ message: "Book not found." });
+    }
+    return res.status(200).send({ message: "Book updated successfully." });
+  } catch (e) {
+    console.error(e.message);
+    res.status(500).send({ message: e.message });
+  }
+});
+
 mongoose
   .connect(mongoDBURL)
   .then(() => {
