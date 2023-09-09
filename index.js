a6b84b6b93f03bc862393f67c3fd9abf9d8e71cf
Dipoengoro <dipoengoro@outlook.com>
2023-09-09 10:09:11 +0700

    feat: add post books

--- index.js
+++ index.js
@@ -1,20 +1,46 @@
 import "dotenv/config";
-import express from "express";
+import express, { response } from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
+import { Book } from "./models/book-models.js";
 
 const app = express();
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   console.log(req);
   return res.status(234).send("Welcome to MERN Stack Tutorial");
 });
 
-app.listen(PORT, () => {
-  console.log(`App is listening to port: ${PORT}`);
+app.post("/books", async (req, res) => {
+  try {
+    if (!req.body.title || !req.body.author || !req.body.publishYear) {
+      return res.status(400).send({
+        message: "Send all required fields: title, author, publishYear.",
+      });
+    }
+    const newBook = {
+      title: req.body.title,
+      author: req.body.author,
+      publishYear: req.body.publishYear,
+    };
+
+    const book = await Book.create(newBook);
+
+    return res.status(201).send(book);
+  } catch (e) {
+    console.error(e.message);
+    response.status(500).send({ message: e.message });
+  }
 });
 
 mongoose
   .connect(mongoDBURL)
-  .then(() => {console.log('App connected to the database');})
+  .then(() => {
+    console.log("App connected to the database");
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
+    });
+  })
   .catch((e) => console.log(e));
